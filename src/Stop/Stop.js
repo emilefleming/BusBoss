a5e64d7136f922ec2247769b921c86b2eda134a5
Emile Mirth Fleming <emile.fleming@gmail.com>
2017-03-11 16:17:55 -0800

    Remove stop map from stop info sidebar

--- src/Stop/Stop.js
+++ src/Stop/Stop.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import Arrival from '../Arrival/Arrival';
 import './Stop.css';
-import StopMap from './StopMap'
 
 export default function Stop(props) {
   const { arrivals, stop, setActiveTrip } = props;
@@ -10,7 +9,6 @@ export default function Stop(props) {
     <div className="Stop">
       <div className="stopInfo">
         <h2>{ stop.name }</h2>
-          <StopMap lat={stop.lat} lng={stop.lon} />
       </div>
       <div className="arrivals" onMouseLeave={ () => {setActiveTrip(null)} }>
         {
