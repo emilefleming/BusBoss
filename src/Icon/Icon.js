e57e73598dfa8c72350d1d9a0468148ae3589420
Emile Mirth Fleming <emile.fleming@gmail.com>
2017-03-15 12:58:04 -0700

    Add mdi icons and wrapper class

---
+++ src/Icon/Icon.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+export default function Icon({ i }) {
+  return <i className={`mdi mdi-${ i }`}></i>
+}
--- src/Trip/Trip.js
+++ src/Trip/Trip.js
@@ -1,35 +1,32 @@
 import React from 'react';
 import './Trip.css';
+import Icon from '../Icon/Icon';
 
-import TripStop from './TripStop'
+import TripStop from './TripStop';
 
 export default function Trip(props) {
   const { trip, stops } = props;
+  const { routeShortName, routeLongName, tripStatus, tripHeadsign } = trip;
   console.log(trip, stops);
   let passedStop = true;
   return (
     <div className="Trip">
       <h2>
-        { trip.routeShortName || trip.routeLongName }
-        <em>{ trip.tripHeadsign }</em>
+        { routeShortName || routeLongName }
+        <em>{ tripHeadsign }</em>
       </h2>
       <div className="schedule">
         <div className="line"></div>
         {
-          stops.map(stop => {
-            if (stop.id === trip.tripStatus.nextStop) {
-              passedStop = false;
-            }
-            return (
-              <TripStop key={ stop.id } stop={ stop }>
-                {
-                  passedStop
-                  ? <div className="stopped">✓</div>
-                  : null
-                }
-              </TripStop>
-            )
-          })
+          stops.map(stop =>
+            <TripStop key={ stop.id } stop={ stop }>
+              {
+                tripStatus.distanceAlongTrip > stop.departure.distanceAlongTrip
+                ? <div className="stopped"><Icon i="checkbox-marked-circle" /></div>
+                : null
+              }
+            </TripStop>
+          )
         }
       </div>
     </div>
