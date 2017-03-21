2019cd9fb2f029077d8b560ab3222d33b8869b7e
Emile Mirth Fleming <emile.fleming@gmail.com>
2017-03-21 14:43:02 -0700

    Restructure header

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -6,14 +6,14 @@ export default function Header(props) {
   return (
     <nav className="Header">
       <div className="navbar">
-        <Link to="/"><div className="navItem">Map</div></Link>
-        <Link to="/favorites"><div className="navItem">Favorites</div></Link>
         <div className="logo">
           <img src="/icons/logo.svg" alt="Logo"/>
         </div>
+        <Link to="/"><div className="navItem">Map</div></Link>
+        <Link to="/favorites"><div className="navItem">Favorites</div></Link>
         {
           props.userData
-          ? <Link to="/account"><div className="navItem">Account</div></Link>
+          ? null
           : <Link to="/login"><div className="navItem">Login</div></Link>
         }
         {
