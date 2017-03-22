24ceb3da9609e9bc17005b34d36b93aeb7ee0d09
Emile Mirth Fleming <emile.fleming@gmail.com>
2017-03-22 08:41:10 -0700

    Wire favorites to socket for updates

--- src/Favorites/Favorites.js
+++ src/Favorites/Favorites.js
@@ -3,6 +3,10 @@ import axios from 'axios';
 import Favorite from './Favorite';
 import './Favorites.css'
 
+import moment from 'moment';
+import io from 'socket.io-client';
+const socket = io();
+
 export default class Favorites extends Component {
   constructor(props) {
     super(props);
@@ -10,6 +14,26 @@ export default class Favorites extends Component {
     this.state = { favorites: [] }
 
     this.removeFavorite = this.removeFavorite.bind(this);
+
+    socket.on('arrivals', data => {
+      console.log(data);
+      this.setState({ animate: false }, () => {
+        const favorites = this.state.favorites.map(favorite => {
+          if (favorite.data.entry.id === data[0].stopId) {
+            favorite.arrivals = data;
+          }
+
+          return favorite;
+        })
+        setTimeout(() => {
+          this.setState({
+            favorites,
+            lastUpdated: moment(),
+            animate: true
+          });
+        }, 0)
+      })
+    });
   }
 
   componentDidMount() {
@@ -27,10 +51,24 @@ export default class Favorites extends Component {
     this.updateFavorites()
   }
 
+  componentWillUnmount() {
+    for (const favorite of this.state.favorites) {
+      socket.off('arrivals');
+      socket.emit('leave', {
+        room: `stop-${favorite.data.entry.id}`
+      });
+    }
+  }
+
   updateFavorites() {
     axios.get(`/api/favorites/${this.props.userData.id}`)
     .then(response => {
-      this.setState({ favorites: response.data, isLoaded: true })
+      for (const favorite of response.data) {
+        socket.emit('room', {
+          room: `stop-${favorite.data.entry.id}`
+        });
+      }
+      this.setState({ favorites: response.data, isLoaded: true, animate: true })
     })
     .catch(err => {
       console.log(err);
@@ -55,6 +93,9 @@ export default class Favorites extends Component {
   render() {
     return (
       <div className="Favorites">
+        <div className="loader">
+          <div className={`loadBar ${this.state.animate ? 'loadAnimation' : ''}`}></div>
+        </div>
         {
           this.state.favorites.map(favorite =>
             <Favorite
