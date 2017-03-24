8debeb79450359f389af71b1118a52bdc2c7000c
Emile Mirth Fleming <emile.fleming@gmail.com>
2017-03-24 10:12:24 -0700

    Render special icons for ferries, BRT, and light rail

--- src/Arrival/Arrival.js
+++ src/Arrival/Arrival.js
@@ -2,16 +2,28 @@ import React, { Component } from 'react';
 import './Arrival.css';
 import moment from 'moment';
 import Timeliness from '../Timeliness/Timeliness'
+import Icon from '../Icon/Icon'
 
 export default function Arrival(props) {
   const { arrival, setHoverTrip, setClickedTrip, lastUpdated } = props;
+  let title = arrival.routeShortName || arrival.routeLongName;
+
+  if (title === 'LINK') {
+    title = <div className="link">LINK</div>
+  }
+  else if (title.match(/Line$/)) {
+    title = <div className="brt"><p>{title.slice(0, 1)}</p></div>
+  }
+  else if (arrival.routeId.match(/^95/)) {
+    title = <div className="ferry"><Icon i='ferry' /></div>
+  }
   return (
     <div
       className="Arrival"
       onClick={ () => { setClickedTrip(arrival)} }
       onMouseEnter={ () => {setHoverTrip(arrival)} }
     >
-      <h3>{ arrival.routeShortName || arrival.routeLongName }</h3>
+      <h3>{ title }</h3>
       <div className="details">
         <h4>{ arrival.tripHeadsign }</h4>
         <h5>
