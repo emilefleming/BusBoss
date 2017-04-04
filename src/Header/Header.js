0e349f8dc11bedcac25c3b6eb8f7b2b0d7f50f0a
Emile Mirth Fleming <emile.fleming@gmail.com>
2017-04-04 10:42:39 -0700

    Show which is the current page on the nav

--- src/Header/Header.js
+++ src/Header/Header.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './Header.css';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 
 export default function Header(props) {
   return (
@@ -10,19 +10,19 @@ export default function Header(props) {
         <img src="/icons/logo.svg" alt="Logo"/>
       </div>
       <div className="navbar">
-        <Link to="/map"><div className="navItem">Map</div></Link>
-        <Link to="/favorites"><div className="navItem">Favorites</div></Link>
+        <NavLink activeClassName="active" to="/map"><div className="navItem">Map</div></NavLink>
+        <NavLink activeClassName="active" to="/favorites"><div className="navItem">Favorites</div></NavLink>
         {
           props.userData
           ? null
-          : <Link to="/login"><div className="navItem">Login</div></Link>
+          : <NavLink activeClassName="active" to="/login"><div className="navItem">Login</div></NavLink>
         }
         {
           props.userData
           ? <div onClick={ props.logOut } className="navItem logOut">
               Log Out
             </div>
-          : <Link to="/signup"><div className="navItem">Signup</div></Link>
+          : <NavLink activeClassName="active" to="/signup"><div className="navItem">Signup</div></NavLink>
         }
 
       </div>
